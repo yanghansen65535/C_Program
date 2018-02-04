bd75565a31fa7257b73ae39299faf8bbae1d5dd7
Unknown <496645649@qq.com>
2018-02-04 23:41:04 +0800

    insertion sort better

--- C_DSandalgorithm/SRC/algorithm_7_sort.c
+++ C_DSandalgorithm/SRC/algorithm_7_sort.c
@@ -11,3 +11,14 @@ int insertion_sort_main(int * input,int length){
 	}
 	return 0;
 }
+int insertion_sort_main_better(int * input,int length){
+	int i,j,temp;
+	for(i=1;i<length;i++){
+		temp=*(input+i);
+		for(j=i-1;j>=0&&*(input+j)>temp;j--){
+			*(input+j+1)=*(input+j);
+		}
+		*(input+j+1)=temp;
+	}
+	return 0;
+}
--- C_DSandalgorithm/SRC/algorithm_7_sort.h
+++ C_DSandalgorithm/SRC/algorithm_7_sort.h
@@ -1,2 +1,3 @@
 #include<stdio.h>
 int insertion_sort_main(int * input,int length);
+int insertion_sort_main_better(int * input,int length);
